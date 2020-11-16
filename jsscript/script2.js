457fdc681e4a0ecf1c7bc7021b3c0b808fd6e0ac
Exilic <tobias.engberg@exilic.se>
2020-11-16 22:13:00 +0100

    Second exercise

---
+++ jsscript/script2.js
@@ -0,0 +1,17 @@
+function changeToFirst() {
+  document.getElementById("aside_1").style.color = "#000000";
+  document.getElementById("aside_2").style.color = "#a8a8a8";
+  document.getElementById("aside_3").style.color = "#a8a8a8";
+}
+
+function changeToSecond() {
+  document.getElementById("aside_1").style.color = "#a8a8a8";
+  document.getElementById("aside_2").style.color = "#000000";
+  document.getElementById("aside_3").style.color = "#a8a8a8";
+}
+
+function changeToThird() {
+  document.getElementById("aside_1").style.color = "#a8a8a8";
+  document.getElementById("aside_2").style.color = "#a8a8a8";
+  document.getElementById("aside_3").style.color = "#000000";
+}
