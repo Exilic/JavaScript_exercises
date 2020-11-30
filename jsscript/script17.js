ac3fe6ee192aaf535ce627e54fe748acabbc82db
Exilic <tobias.engberg@exilic.se>
2020-11-30 21:28:29 +0100

    Seventeenth exercise

---
+++ jsscript/script17.js
@@ -0,0 +1,8 @@
+var inputString;
+var latestKey;
+var inputRange = document.getElementsByTagName("input")[0];
+
+inputRange.addEventListener("input", function (e) {
+  inputRange.previousElementSibling.innerHTML =
+    "256 multiplied by " + inputRange.value + " is " + inputRange.value * 256;
+});
