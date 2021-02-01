0dea3712e9487313049a90b659d623350b6f0d3b
Exilic <tobias.engberg@exilic.se>
2021-02-01 23:51:53 +0100

    Eightieth exercise

---
+++ jsscript/script80.js
@@ -0,0 +1,11 @@
+document.getElementsByTagName("h1")[0].addEventListener("click", (e) => {
+  document.getElementsByTagName("h1")[0].requestFullscreen();
+});
+
+document.getElementsByTagName("h2")[0].addEventListener("click", (e) => {
+  document.getElementsByTagName("h2")[0].requestFullscreen();
+});
+
+document.getElementsByTagName("header")[0].addEventListener("click", (e) => {
+  document.getElementsByTagName("header")[0].requestFullscreen();
+});
